e4be66ccdee2bbb5e33288f663f5612605e406b0
Matysuu <82321793+Matysuu@users.noreply.github.com>
2021-05-09 13:05:14 +0000

    Nauka pythona

---
+++ zadanie11.py
@@ -0,0 +1,9 @@
+magazyn = [
+    {'nazwa': 'mleko', 'cena': 12},
+    {'nazwa': 'czekolada', 'cena': 13},
+    {'nazwa': 'tyskie', 'cena': 8},
+    {'nazwa': 'audi', 'cena': 20000},
+]
+
+for pozycja in magazyn:
+    print(pozycja['nazwa'])
