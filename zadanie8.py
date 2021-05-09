be9519148cadd4dad0591986726221b2ddedeac0
Matysuu <82321793+Matysuu@users.noreply.github.com>
2021-05-09 12:22:35 +0000

    Nauka pythona

--- zadanie8.py
+++ zadanie8.py
@@ -1,13 +1,23 @@
-# samochody = ['syrena', 'poloneza']
+# samochody = ['syrena', 'polonez']
+#
 #
 # print(samochody[0])
 # print(samochody[1])
 #
+# print("-------------")
+#
 # for s in samochody:
 #     print(s)
 #
+# print("==============")
+#
 # for idx in range( len(samochody) ) :
 #     print("idx: " + str(idx) + " : " + samochody[idx])
+#
+# print("++++++++++++++++")
+#
+# for idx in range(len(samochody)):
+#     print("{0} {1}".format(idx, samochody[idx]))
 
 imie = ['Aga', 'Michal']
 
@@ -19,3 +29,6 @@ for i in imie:
 
 for idx in range ( len(imie) ) :
     print("idx: " + str(idx) + " : " + imie[idx])
+
+for idx in range(len(imie)):
+    print("{0} {1}".format(idx, imie[idx]))
