b4a7f2e321484c9698db1345f8d35b6f42656c3e
Matysuu <82321793+Matysuu@users.noreply.github.com>
2021-05-09 13:01:41 +0000

    Nauka pythona

--- zadanie10.py
+++ zadanie10.py
@@ -2,15 +2,26 @@ samolot = {'name': 'boeing',
            'przebieg': 10000,
            'type': 'pasazerski'}
 
-print(samolot['name'])
-print(samolot['type'])
+# print(samolot['name'])
+# print(samolot['type'])
 
-print(samolot['nieznany_klucz'])
-print(samolot.get('nieznany_klucz'))
+# print(samolot.get('nieznany_klucz'))
 
 for key in samolot:
+    # print(key)
     print('{0}:{1}'.format(key, samolot[key]))
 
-in python2 samolot.iteritems()
+print('=========================')
+
 for key, value in samolot.items():
     print("{0}:{1}".format(key, value))
+
+print('=========================')
+
+if 'name' in samolot:
+    print("YES")
+
+print('==========================')
+
+if 'kola' not in samolot:
+    print('NO')
