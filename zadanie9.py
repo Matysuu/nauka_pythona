8bafca908997006af33f7eae3c71df438e768529
Matysuu <82321793+Matysuu@users.noreply.github.com>
2021-05-09 12:38:47 +0000

    Nauka pythona

--- zadanie9.py
+++ zadanie9.py
@@ -1,8 +1,14 @@
-samochody = ['syrena', 'polonez']
-ilosc = [3, 5]
+# samochody = ['syrena', 'polonez']
+# ilosc = [3, 5]
+#
+# for idx in range( len(samochody)) :
+#     print("idx: {0} {1}".format(idx, samochody[idx]))
+#     print(samochody[idx] + " ma ilosc drzwi " + str(ilosc[idx]))
 
-for idx in range( len(samochody)) :
-    print("idx: {0} {1}".format(idx, samochody[idx]))
-    print(samochody[idx] + " ma ilosc drzwi " + str(ilosc[idx]))
 
-    
+slodycze = ['Aga', 'Michal']
+ilosc = [1, 12]
+
+for idx in range( len(slodycze)) :
+    print('idx: {0} {1}'.format(idx, slodycze[idx]))
+    print(slodycze[idx] + " ma ilosc cukieraskow " + str(ilosc[idx]))
